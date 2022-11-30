51c4220cfc6425fa089e9519dc2079c6e6aaddd0
Thierry Mugisha <mugishathierry65@gmail.com>
2022-11-30 17:13:55 +0200

    Add files via upload

---
+++ Question2/four.js
@@ -0,0 +1,22 @@
+const item = [
+    { name: 'Bike', price: 100 },
+    { name: 'TV', price: 200 },
+    { name: 'Album', price: 10 },
+    { name: 'Book', price: 5 },
+    { name: 'Phone', price: 500 },
+    { name: 'Computer', price: 1000 }
+]
+
+var x = 0
+var somme = 0
+while (x < 6) {
+    if (item[x].price < 10) {
+        x++
+    }
+    else {
+        somme = somme + item[x].price
+        x++;
+    }
+}
+
+console.log(`Full price without product under 10$ is ${somme} $`)
---
+++ Question2/one.js
@@ -0,0 +1,21 @@
+const item = [
+    { name: 'Bike', price: 100 },
+    { name: 'TV', price: 200 },
+    { name: 'Album', price: 10 },
+    { name: 'Book', price: 5 },
+    { name: 'Phone', price: 500 },
+    { name: 'Computer', price: 1000 }
+]
+
+var x = 1
+var i = 0
+var all = item[0].price
+while (x < 6) {
+    if (item[x].price < all) {
+        i = x
+        x++
+    } else {
+        x++
+    }
+}
+console.log(`Cheaper is ${item[i].name} with ${item[i].price} $`)
